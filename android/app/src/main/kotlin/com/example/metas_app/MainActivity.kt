dfa48074865a58a745baec6bc37fdab61a19b19d
unknown <freb2060@gmail.com>
2026-01-04 21:57:08 -0500

    Entities and repositories implementations for user and firebase authentication

---
+++ android/app/src/main/kotlin/com/example/metas_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.metas_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/features/domain/mappers/app_user.mapper.dart
@@ -0,0 +1,15 @@
+import 'package:metas_app/features/domain/entities/app_user.dart';
+
+AppUser appUserFromJson(Map<String, dynamic> json) {
+  return AppUser(
+    uid: json['uid'] ?? '',
+    email: json['email'] ?? '',
+  );
+}
+
+Map<String, dynamic> appUserToJson(AppUser user) {
+  return {
+    'uid': user.uid,
+    'email': user.email,
+  };
+}
